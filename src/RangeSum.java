c674cf44160af4933af89806db4ea6351d8c6e86
octopus98 <octopus9298@gmail.com>
2022-11-07 22:33:38 -0800

    Initial commit

--- src/RangeSum.java
+++ src/RangeSum.java
@@ -1,2 +1,17 @@
 public class RangeSum {
+    private int rangeSum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+
+        if(root == null) return 0;
+
+        rangeSumBST(root.left,low,high);
+
+        if(root.val >= low && root.val <= high) {
+            rangeSum += root.val;
+        }
+
+        rangeSumBST(root.right,low,high);
+
+        return rangeSum;
+    }
 }
